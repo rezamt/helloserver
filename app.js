a3b98d3dfd70f5e96c6f410d0be887e3edd2213a
reza <rezamt@gmail.com>
2019-05-04 12:27:24 +1000

    first commit

--- app.js
+++ app.js
@@ -1,7 +1,5 @@
 const http = require('http');
 
-const hostname = '127.0.0.1';
-const port = process.env.PORT || 5000
 
 const server = http.createServer((req, res) => {
   // console.log("Incoming Request "+ req.method+ " : "+ req.url)
@@ -10,6 +8,9 @@ const server = http.createServer((req, res) => {
   res.end('Hello World\n');
 });
 
-server.listen(port, hostname, () => {
-  console.log(`Server running at http://${hostname}:${port}/`);
+var server_port = process.env.YOUR_PORT || process.env.PORT || 80;
+var server_host = process.env.YOUR_HOST || '0.0.0.0';
+server.listen(server_port, server_host, function() {
+    console.log('Listening on port %d', server_port);
 });
+
